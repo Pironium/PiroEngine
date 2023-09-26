4e6c26b58e9f8a136d70ee0744a58fa302d25e8d
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 04:20:27 +0400

    engine/rendering/models.py uploaded!

---
+++ engine/rendering/models.py
@@ -0,0 +1,17 @@
+class Piro3DModel:
+    def __init__(self, model_data):
+        self.model_data = model_data
+        self.texture = None
+        self.shader = None
+
+    def load_texture(self, texture_path):
+        # Code to load and apply a texture to the 3D model
+        pass
+
+    def set_shader(self, shader):
+        # Code to assign a custom shader to the 3D model
+        pass
+
+    def render(self):
+        # Code to render the 3D model with the assigned texture and shader
+        pass
