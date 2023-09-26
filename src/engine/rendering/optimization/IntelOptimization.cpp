830a4aaed6ed4c121532d70df3a5a1fa73ad0814
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 20:48:16 +0400

    src/engine/rendering/optimization/IntelOptimization.cpp uploaded!

---
+++ src/engine/rendering/optimization/IntelOptimization.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "PiroEngine.h"
+
+void IntelOptimization::optimizeRendering(PiroScene* scene) {
+    // Implementing Intel-specific optimizations for rendering
+    // ...
+    std::cout << "Intel optimization applied!" << std::endl;
+}
