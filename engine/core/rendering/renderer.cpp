fce2708b1408fce0eca08a1425219e21bd382928
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 07:43:32 +0400

    engine/core/rendering/renderer.cpp uploaded!

---
+++ engine/core/rendering/renderer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class PiroRenderer {
+public:
+    PiroRenderer() {
+        std::cout << "Initializing PiroRenderer" << std::endl;
+    }
+
+    void renderScene() {
+        // Complex rendering logic goes here
+        std::cout << "Rendering the scene with PiroRenderer" << std::endl;
+    }
+};
