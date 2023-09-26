334dd80f6ddb306848ccae69aa5c99ff664ad7f0
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 16:09:07 +0400

    PiroEngine/CompileSupport.cpp uploaded!

---
+++ PiroEngine/CompileSupport.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+// Класс, отвечающий за компиляцию игры для разных платформ
+class CompilationSupport {
+public:
+    void compileForWindows() {
+        std::cout << "Compiling for Windows..." << std::endl;
+        // Добавьте здесь код для компиляции под Windows
+    }
+
+    void compileForAndroid() {
+        std::cout << "Compiling for Android..." << std::endl;
+        // Добавьте здесь код для компиляции под Android
+    }
+
+    void compileForiPhone() {
+        std::cout << "Compiling for iPhone..." << std::endl;
+        // Добавьте здесь код для компиляции под iPhone
+    }
+
+    void compileForMacOS() {
+        std::cout << "Compiling for macOS..." << std::endl;
+        // Добавьте здесь код для компиляции под macOS
+    }
+
+    void compileForLinux() {
+        std::cout << "Compiling for Linux..." << std::endl;
+        // Добавьте здесь код для компиляции под Linux
+    }
+
+    void compileForPS4() {
+        std::cout << "Compiling for PS4..." << std::endl;
+        // Добавьте здесь код для компиляции под PS4
+    }
+
+    void compileForXboxOne() {
+        std::cout << "Compiling for Xbox One..." << std::endl;
+        // Добавьте здесь код для компиляции под Xbox One
+    }
+};
