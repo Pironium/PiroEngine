3b1de430ea7fc7a15484e02aa808f31b9e249654
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 16:11:15 +0400

    Engine/CloudRendering.cpp uploaded!

---
+++ Engine/CloudRendering.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+class CloudRendering {
+public:
+    static void renderVolumetricClouds() {
+        std::cout << "Rendering volumetric clouds..." << std::endl;
+        // Добавьте здесь код для рендеринга объемных облаков
+    }
+};
