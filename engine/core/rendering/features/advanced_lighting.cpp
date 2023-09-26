eb322a0d95fa5756f4bd855d41ac30c48e2aec9c
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 09:06:25 +0400

    engine/core/rendering/features/advanced_lighting.cpp uploaded!

---
+++ engine/core/rendering/features/advanced_lighting.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "math_helpers.h"
+
+namespace PiroEngine {
+
+    class AdvancedLighting {
+    public:
+        AdvancedLighting() {}
+
+        void computeGlobalIllumination() {
+            // Complex global illumination calculations here
+            std::cout << "Performing advanced global illumination calculations...\n";
+            // Code for advanced lighting effects
+            // ...
+        }
+
+        void applyPostProcessingEffects() {
+            // Apply post-processing effects
+            std::cout << "Applying post-processing effects...\n";
+            // Code for post-processing effects
+            // ...
+        }
+    };
+
+} // namespace PiroEngine
