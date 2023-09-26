dac9a07e162ced33ccc92bf0a2eea3ede540d652
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 20:51:43 +0400

    optimization.c uploaded!

---
+++ optimization.c
@@ -0,0 +1,23 @@
+// Код оптимизации для процессоров Intel и AMD
+#include <stdio.h>
+#include <stdlib.h>
+
+// Функция оптимизации для процессоров Intel
+void optimizeForIntel() {
+    // Ваш сложный код оптимизации для процессоров Intel здесь
+    printf("Оптимизация для процессоров Intel выполнена.\n");
+}
+
+// Функция оптимизации для процессоров AMD
+void optimizeForAMD() {
+    // Ваш сложный код оптимизации для процессоров AMD здесь
+    printf("Оптимизация для процессоров AMD выполнена.\n");
+}
+
+int main() {
+    // Вызываем оптимизацию для Intel и AMD
+    optimizeForIntel();
+    optimizeForAMD();
+
+    return 0;
+}
