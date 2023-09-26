39ca6be8ba8356b186f3d7c40682f70de931a2ac
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 06:40:25 +0400

    src/graphics/shadow_mapping.cpp uploaded!

---
+++ src/graphics/shadow_mapping.cpp
@@ -0,0 +1,8 @@
+#include "shadow_mapping.h"
+
+// Complex shadow mapping algorithm implementation
+void ShadowMapping::GenerateShadows(Scene* scene, Light* light, RenderTarget* shadowMap) {
+    // Implement advanced shadow mapping algorithm here
+    // This code generates realistic shadows for 3D objects
+    // ...
+}
