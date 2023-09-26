39d460f5ab607c5679079c2d4901bb7c5e751a04
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 19:38:08 +0400

    graphics_optimization/nvidia_optimization.c uploaded!

---
+++ graphics_optimization/nvidia_optimization.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// Этот файл будет содержать оптимизации специфичные для Nvidia видеокарт
+
+void optimizeForNvidia() {
+    // Реализация оптимизаций для Nvidia видеокарт
+    printf("Optimizing for Nvidia...\n");
+    // Добавьте здесь специфичные оптимизации для Nvidia
+}
