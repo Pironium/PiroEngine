235d20b1556b1fa8fcfb20030d94fc525ce14640
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 12:17:36 +0400

    engine/core/graphics/renderer.cpp uploaded!

---
+++ engine/core/graphics/renderer.cpp
@@ -0,0 +1,9 @@
+#include "renderer.h"
+
+void PiroEngine::Renderer::renderWaterSimulation() {
+    // Complex water simulation rendering code goes here
+    // This feature simulates realistic water behavior in 3D scenes
+    // ...
+}
+
+// Rest of the renderer implementation...
