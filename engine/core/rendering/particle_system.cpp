7c6a802e5a8c15654b63e2f929370c791f06e454
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 05:42:27 +0400

    engine/core/rendering/particle_system.cpp uploaded!

---
+++ engine/core/rendering/particle_system.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+class ParticleSystem {
+public:
+    ParticleSystem() {
+        // Initialize particle system here
+    }
+
+    void addParticle(float x, float y, float z) {
+        // Add a particle at the specified position
+        // Implement complex particle logic here
+    }
+
+    void update(float deltaTime) {
+        // Update particle system state based on time
+        // Implement advanced physics and rendering calculations
+    }
+
+    void render() {
+        // Render particles with advanced graphics techniques
+        // Utilize shaders and GPU for particle rendering
+    }
+};
