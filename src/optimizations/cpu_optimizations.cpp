eb8fb6a46e0981ad2dece060265e9ee0e7b2cccf
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 21:11:41 +0400

    src/optimizations/cpu_optimizations.cpp uploaded!

---
+++ src/optimizations/cpu_optimizations.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void optimizeForIntel() {
+    // Intel-specific optimizations
+    // ...
+    std::cout << "Optimizing for Intel processors...\n";
+}
+
+void optimizeForAMD() {
+    // AMD-specific optimizations
+    // ...
+    std::cout << "Optimizing for AMD processors...\n";
+}
