f80734c816ba8837ac92a5c6e5ff9c58fad0d441
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 06:53:33 +0400

    engine/resources_manager.py uploaded!

---
+++ engine/resources_manager.py
@@ -0,0 +1,17 @@
+class ResourceManager:
+    def __init__(self):
+        self.textures = {}
+        self.models = {}
+        self.sounds = {}
+
+    def load_texture(self, path):
+        # Загрузка текстуры из файла
+        pass
+
+    def load_model(self, path):
+        # Загрузка модели из файла
+        pass
+
+    def load_sound(self, path):
+        # Загрузка звука из файла
+        pass
