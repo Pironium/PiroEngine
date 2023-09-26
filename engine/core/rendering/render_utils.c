bc71656310e95a44ea69854bd3fffd76aa1d77a3
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 07:03:30 +0400

    engine/core/rendering/render_utils.c uploaded!

---
+++ engine/core/rendering/render_utils.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Function to calculate complex lighting effects
+void calculateComplexLighting(float* vertexData, int vertexCount, float* lightData, int lightCount) {
+    // Complex lighting calculations here
+    // ...
+}
+
+// Function to apply advanced shaders
+void applyAdvancedShaders(float* vertexData, int vertexCount, char* shaderCode, int shaderLength) {
+    // Advanced shader application here
+    // ...
+}
