f7f91450c78bf91b1c23d42a5050a4f528aa6990
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 14:41:02 +0400

    debug/debug_utils.cpp uploaded!

---
+++ debug/debug_utils.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+void trackMemoryUsage() {
+    // Code to track memory usage
+    // This could involve system-specific API calls to monitor memory usage.
+    // For simplicity, let's simulate memory tracking.
+    std::cout << "Tracking memory usage...\n";
+}
+
+void trackCPUUsage() {
+    // Code to track CPU usage
+    // This could involve system-specific API calls to monitor CPU usage.
+    // For simplicity, let's simulate CPU tracking.
+    std::cout << "Tracking CPU usage...\n";
+}
+
+void trackGPUUsage() {
+    // Code to track GPU usage
+    // This could involve system-specific API calls to monitor GPU usage.
+    // For simplicity, let's simulate GPU tracking.
+    std::cout << "Tracking GPU usage...\n";
+}
