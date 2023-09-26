5707bdf7743053c4fa1d6ca5df7807fa16e3922b
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 05:25:30 +0400

    engine/core/rendering/render_manager.cpp uploaded!

---
+++ engine/core/rendering/render_manager.cpp
@@ -0,0 +1,11 @@
+#include "render_manager.h"
+
+RenderManager::RenderManager() {
+    // Инициализация системы рендеринга
+}
+
+void RenderManager::RenderScene(Scene* scene) {
+    // Рендеринг всей сцены
+}
+
+// Другие методы класса RenderManager
