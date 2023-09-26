786f318e473e752c8875c8590c9b68906dc0f96d
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 16:04:03 +0400

    engine/src/graphics/rendering_system.cpp uploaded!

---
+++ engine/src/graphics/rendering_system.cpp
@@ -0,0 +1,87 @@
+#include "rendering_system.h"
+#include <iostream>
+
+void RenderingSystem::initialize() {
+    // Инициализация графической системы
+    // ...
+}
+
+void RenderingSystem::renderScene(Scene* scene) {
+    // Рендеринг сцены
+    // ...
+}
+
+void RenderingSystem::setResolution(int width, int height) {
+    // Установка разрешения
+    // ...
+}
+
+void RenderingSystem::enablePostProcessing(bool enable) {
+    // Включение/отключение постобработки
+    // ...
+}
+
+void RenderingSystem::setPostProcessingEffect(EffectType effect) {
+    // Установка эффекта постобработки
+    // ...
+}
+
+void RenderingSystem::setAntiAliasingLevel(int level) {
+    // Установка уровня сглаживания
+    // ...
+}
+
+void RenderingSystem::setShadowQuality(QualityLevel quality) {
+    // Установка качества теней
+    // ...
+}
+
+void RenderingSystem::setReflectionQuality(QualityLevel quality) {
+    // Установка качества отражений
+    // ...
+}
+
+void RenderingSystem::setAnisotropicFiltering(bool enable) {
+    // Включение/отключение анизотропной фильтрации
+    // ...
+}
+
+void RenderingSystem::setVSync(bool enable) {
+    // Включение/отключение вертикальной синхронизации
+    // ...
+}
+
+void RenderingSystem::setHDR(bool enable) {
+    // Включение/отключение HDR
+    // ...
+}
+
+void RenderingSystem::setBloomIntensity(float intensity) {
+    // Установка интенсивности бликов
+    // ...
+}
+
+void RenderingSystem::setSSAO(bool enable) {
+    // Включение/отключение SSAO
+    // ...
+}
+
+void RenderingSystem::setMotionBlur(bool enable) {
+    // Включение/отключение эффекта движения
+    // ...
+}
+
+void RenderingSystem::setDepthOfField(bool enable) {
+    // Включение/отключение эффекта глубины резкости
+    // ...
+}
+
+void RenderingSystem::setFog(FogType type, Color color, float density) {
+    // Установка параметров тумана
+    // ...
+}
+
+void RenderingSystem::setAmbientLight(Color color, float intensity) {
+    // Установка параметров окружающего освещения
+    // ...
+}
