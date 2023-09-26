9a2bff226df148270df5a1ecfda854bb0fb3baff
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 16:02:42 +0400

    platform_compiler.py uploaded!

---
+++ platform_compiler.py
@@ -0,0 +1,29 @@
+# platform_compiler.py - Файл для компиляции игр под разные платформы
+
+def compile_for_windows(game_name):
+    # Код для компиляции игры под Windows
+    print(f"Компиляция игры '{game_name}' под Windows...")
+
+def compile_for_android(game_name):
+    # Код для компиляции игры под Android
+    print(f"Компиляция игры '{game_name}' под Android...")
+
+def compile_for_iphone(game_name):
+    # Код для компиляции игры под iPhone
+    print(f"Компиляция игры '{game_name}' под iPhone...")
+
+def compile_for_macos(game_name):
+    # Код для компиляции игры под macOS
+    print(f"Компиляция игры '{game_name}' под macOS...")
+
+def compile_for_linux(game_name):
+    # Код для компиляции игры под Linux
+    print(f"Компиляция игры '{game_name}' под Linux...")
+
+def compile_for_ps4(game_name):
+    # Код для компиляции игры под PS4
+    print(f"Компиляция игры '{game_name}' под PS4...")
+
+def compile_for_xbox_one(game_name):
+    # Код для компиляции игры под Xbox One
+    print(f"Компиляция игры '{game_name}' под Xbox One...")
