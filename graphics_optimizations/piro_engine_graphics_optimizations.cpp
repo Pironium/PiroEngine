a2702d6cdf08bef99fdda8f35c9125f5f917fe5e
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 19:56:06 +0400

    graphics_optimizations/piro_engine_graphics_optimizations.cpp uploaded!

---
+++ graphics_optimizations/piro_engine_graphics_optimizations.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+// Определение новой функции для оптимизации графики на видеокартах
+void optimizeGraphicsForNvidiaAndAmd(std::vector<int>& frameData) {
+    // Здесь будет сложный и длинный код оптимизации
+    // для видеокарт от NVIDIA и AMD
+    for (int i = 0; i < frameData.size(); ++i) {
+        // Выполним оптимизацию для каждого кадра данных
+        // на видеокартах NVIDIA и AMD
+        frameData[i] = frameData[i] * 2; // Пример оптимизации
+    }
+}
+
+int main() {
+    std::vector<int> frameData = { /* данные кадра */ };
+
+    // Вызываем новую функцию оптимизации
+    optimizeGraphicsForNvidiaAndAmd(frameData);
+
+    // Другой код движка...
+    
+    std::cout << "PiroEngine: Graphics optimization for Nvidia and AMD completed!" << std::endl;
+    
+    return 0;
+}
