081d9037e9d349e9d3de95c83d62935c7248b19a
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 21:03:43 +0400

    graphics_utils.cpp uploaded!

---
+++ graphics_utils.cpp
@@ -0,0 +1,10 @@
+// graphics_utils.cpp
+
+#include "graphics_utils.h"
+#include "piroengine.h"
+
+// Реализация новой функции для работы с графикой
+void PiroEngine::drawAdvanced3DModel(Advanced3DModel model) {
+    // Ваш код для рисования сложных 3D моделей здесь
+    // Используем оптимизации под процессоры Intel и AMD
+}
