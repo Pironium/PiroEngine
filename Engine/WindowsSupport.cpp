82a06667548a7fbc1e449395cbde62c494f338ae
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 16:11:01 +0400

    Engine/WindowsSupport.cpp uploaded!

---
+++ Engine/WindowsSupport.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+class WindowsSupport {
+public:
+    static void compileForWindows() {
+        std::cout << "Compiling game for Windows..." << std::endl;
+        // Добавьте здесь код для компиляции игры под Windows
+    }
+};
