1e47cc9b416c2163e29fc560fce36658e55b3c54
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 10:49:29 +0400

    engine/core/rendering/particle_system_renderer.js uploaded!

---
+++ engine/core/rendering/particle_system_renderer.js
@@ -0,0 +1,25 @@
+class ParticleSystemRenderer {
+  constructor() {
+    // Initialize particle system renderer
+  }
+
+  setParticleSystem(particleSystem) {
+    // Set the particle system to render
+  }
+
+  setShader(shader) {
+    // Set the shader for rendering particles
+  }
+
+  setTexture(texture) {
+    // Set the texture for particles
+  }
+
+  render() {
+    // Implement advanced particle rendering logic here
+    // This code generates complex particle rendering effects
+    // ...
+  }
+}
+
+export default ParticleSystemRenderer;
