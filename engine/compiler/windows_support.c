604359531bd00ee8f519de7570e4ca9d6af2da3f
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 16:04:38 +0400

    engine/compiler/windows_support.c uploaded!

---
+++ engine/compiler/windows_support.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int compileForWindows(const char* gameName, const char* sourceCode) {
+    // Здесь добавим сложную логику для компиляции игры для Windows.
+    printf("Компиляция игры '%s' для Windows завершена успешно.\n", gameName);
+    return 0;
+}
