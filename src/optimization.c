3c977c9b139bdfdf7255692ab049ebfa47d8e924
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-09-26 20:52:14 +0400

    src/optimization.c uploaded!

---
+++ src/optimization.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to optimize for Intel processors
+void optimizeForIntel() {
+    // Complex optimization code specific to Intel processors
+    printf("Optimizing for Intel processors...\n");
+    // ...
+}
+
+// Function to optimize for AMD processors
+void optimizeForAMD() {
+    // Complex optimization code specific to AMD processors
+    printf("Optimizing for AMD processors...\n");
+    // ...
+}
+
+int main() {
+    optimizeForIntel();
+    optimizeForAMD();
+    return 0;
+}
